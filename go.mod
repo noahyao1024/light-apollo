f03b0e8692d2670d7ffdf20b357d2893a52cc712
Noah Yao <hi@noahyao.me>
2023-12-27 17:02:56 +0800

    Read db.

--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.18
 require (
 	github.com/gin-gonic/gin v1.9.1
 	github.com/joho/godotenv v1.5.1
+	github.com/patrickmn/go-cache v2.1.0+incompatible
 	gorm.io/driver/mysql v1.5.2
 	gorm.io/gorm v1.25.5
 )
--- handler/v1.go
+++ handler/v1.go
@@ -1,16 +1,28 @@
 package handler
 
 import (
+	"encoding/json"
+	"fmt"
 	"light-apollo/storage"
+	"time"
 
 	"github.com/gin-gonic/gin"
+	"github.com/patrickmn/go-cache"
 )
 
+const lruTTL = 60 * time.Second
+
+var lru *cache.Cache
+
+func init() {
+	lru = cache.New(lruTTL, 10*time.Minute)
+}
+
 type Release struct {
-	AppID          string `gorm:"primaryKey" json:"appId"`
-	Cluster        string `gorm:"primaryKey" json:"cluster"`
-	Namespace      string `gorm:"primaryKey" json:"namespace"`
-	ReleaseKey     string `json:"releaseKey"`
+	AppID          string `gorm:"primaryKey;Column:AppId" json:"appId"`
+	Cluster        string `gorm:"Column:ClusterName"`
+	Namespace      string `gorm:"Column:NamespaceName"`
+	ReleaseKey     string `gorm:"Column:ReleaseKey"`
 	Configurations string `gorm:"type:text" json:"configurations"`
 }
 
@@ -24,8 +36,21 @@ func Configs(c *gin.Context) {
 	cluster := c.Param("cluster")
 	namespace := c.Param("namespace")
 
+	// Get from cache.
+	key := fmt.Sprintf("%s:%s:%s", appID, cluster, namespace)
+	if raw, exists := lru.Get(key); exists {
+		if data, ok := raw.(map[string]interface{}); ok {
+			c.JSON(200, data)
+			return
+		}
+	}
+
 	releases := make([]Release, 0)
-	err := storage.GetDB().Where(&Release{}).Find(&releases).Error
+	err := storage.GetDB().Where(&Release{
+		AppID:     appID,
+		Cluster:   cluster,
+		Namespace: namespace,
+	}).Find(&releases).Error
 	if err != nil {
 		c.JSON(500, gin.H{
 			"message": err.Error(),
@@ -33,10 +58,18 @@ func Configs(c *gin.Context) {
 		return
 	}
 
-	c.JSON(200, gin.H{
-		"app_id":    appID,
-		"cluster":   cluster,
-		"namespace": namespace,
-		"releases":  releases,
-	})
+	for _, release := range releases {
+		if release.AppID == appID && release.Cluster == cluster && release.Namespace == namespace {
+			data := make(map[string]interface{})
+			json.Unmarshal([]byte(release.Configurations), &data)
+
+			// Set to cache.
+			lru.Set(key, data, lruTTL)
+
+			c.JSON(200, data)
+			return
+		}
+	}
+
+	c.JSON(200, gin.H{})
 }
